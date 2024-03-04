9638008e7b69cb0b5ea63b213acea64481d4e8b7
David Gozlan <davidgozlan@Davids-MacBook-Air.local>
2024-03-04 15:52:33 -0500

    firstC

---
+++ category_app/main.py
@@ -0,0 +1,58 @@
+import openai
+import json
+import os
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+# Set your OpenAI API key here
+api_key = os.getenv("OPENAI_KEY")
+openai.api_key = api_key
+
+# Define the ProductDescription model
+class ProductDescription(BaseModel):
+    description: str
+    
+# Function to generate product categories
+def generate_product_categories(description: str):
+    categories = {}
+    all_categories = []
+    for d in description:
+        # Set up prompt for ChatGPT
+        prompt = f"Based on the following product description generate a raw JSON array of product categories based on the Google Product Category Taxanomy :\n\n{d}:"
+        # Generate categories using ChatGPT
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            response_format={ "type": "json_object" },
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=1000
+        )
+        # Extract categories from the response
+        if response.choices:
+            generated_text = response.choices[0].message.content
+            # Convert the generated JSON string to a Python dictionary
+            categories = json.loads(generated_text)
+            # Append the categories to the list
+            all_categories.append(categories.get("categories"))
+        else:
+            raise HTTPException(status_code=response.status_code, detail="Failed to generate categories.")
+        
+    return all_categories
+
+# Function to generate product category
+@app.post("/generate-product-category/")
+async def get_product_categories(product: ProductDescription):
+    # Generate product categories from product descriptions
+    categories = generate_product_categories([product.description])
+    return {"categories": categories}
+
+# Function to generate product categories
+@app.post("/generate-products-categories/")
+async def get_products_categories(products: list[ProductDescription]):
+    # Generate product categories from product descriptions
+    categories = generate_product_categories([p.description for p in products])
+    return {"categories": categories}
